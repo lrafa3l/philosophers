28c1eca24667457d6fd3957e375a8fad0e96ac72
Lando Rafael <lrafael@c1r7s10.42luanda.com>
2024-10-02 07:34:42 +0100

    first commit, doing the basics

---
+++ philo/philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lrafael <lrafael@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/02 07:05:17 by lrafael           #+#    #+#             */
+/*   Updated: 2024/10/02 07:08:53 by lrafael          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <pthread.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <sys/time.h>
+# include <unistd.h>
+
+#endif
